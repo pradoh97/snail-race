54735fe652946da822655ef3288876a4679b262d
pradoh97 <pradoh97@gmail.com>
2025-11-03 14:17:49 -0300

    Add game over screen

--- level.gd
+++ level.gd
@@ -2,10 +2,17 @@ extends Node2D
 
 var winner: String = ""
 
+func _ready():
+	$UI.restart_pressed.connect(_on_restart_pressed)
+	var racer_number = 1
+	for racer: Racer in $Racers.get_children():
+		racer.racer_number = racer_number
+		racer_number += 1
+
 func _on_finish_line_body_entered(body):
 	if not winner:
-		winner = body.name
-
+		winner = str(body.racer_number)
+		$UI.race_over(winner)
 
 func _on_full_stop_body_entered(body):
 	(body as Racer).race_over = true
@@ -13,3 +20,7 @@ func _on_full_stop_body_entered(body):
 func start_race():
 	for racer: Racer in $Racers.get_children():
 		racer.set_physics_process(true)
+
+func _on_restart_pressed():
+	winner = ""
+	get_tree().reload_current_scene()
